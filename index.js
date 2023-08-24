b99c30c80a2d64f47f5b5748d00e4eb08b412b70
mazyogz <yogiprass11@gmail.com>
2023-08-24 21:37:49 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
